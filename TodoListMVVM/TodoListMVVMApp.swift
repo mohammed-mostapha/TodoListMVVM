f71ab9151e9348fd4dacd15cb7359377a9f2c7c3
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2023-09-15 02:26:29 +0300

    Initial Commit

---
+++ TodoListMVVM/TodoListMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListMVVMApp.swift
+//  TodoListMVVM
+//
+//  Created by Mohamed Mostapha on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
